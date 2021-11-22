c61f727c0628f78cadd3a7bcc9f673a4519ab60b
Pavel_khlebchick <pavelkhlebchik@gmail.com>
2021-11-22 23:17:11 +0300

    Добавляет пропсы для просмотра задач списком, в режиме редактирования и вешает обработчики на переключение статуса задач и выбора просмотра карточки задачии

--- src/components/Item/index.js
+++ src/components/Item/index.js
@@ -1,16 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+import cn from 'classnames';
 
 import s from './item.module.css';
-// eslint-disable-next-line react/prop-types
-const Item = ({ title, description, status }) => (
-  <div className={s.task}>
-    <button className={s.task_status}>{status}</button>
+
+const Item = ({
+  handleClickCard, title, description, status, value, listMode, editMode,
+}) => {
+  const [isDone, setIsDone] = useState(status);
+
+  const handleClick = () => handleClickCard && handleClickCard(value);
+
+  const handleClickStatus = () => {
+    setIsDone((prevstate) => !prevstate);
+  };
+  return <div className={s.task}>
+    <button className={cn(s.task_status, {
+      [s.task_status_completed]: isDone === true,
+      [s.task_status_not_completed]: isDone === false,
+    })}
+      onClick={handleClickStatus}></button>
     <section className={s.task_inner}>
-    <h2 className={s.task_title}>{title}</h2>
-    <p className={s.task_description}>{description}</p>
+      <h2 className={s.task_title}>{title}</h2>
+      <p className={s.task_description}>{description}</p>
     </section>
-    <button className={s.task_view_btn}>View</button>
-  </div>
-);
+    {
+      listMode
+      && <button
+        className={s.task_view_btn}
+        onClick={handleClick}
+        value={title}
+      >View</button>
+    }
+
+    {
+      editMode
+      && <div>
+        <button>
+          Update
+        </button>
+        <button>
+          Delete
+        </button>
+      </div>
+    }
+  </div>;
+};
 
 export default Item;
