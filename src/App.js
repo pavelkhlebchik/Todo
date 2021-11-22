cd16aa3348b79feb01737fff7fe2cfbc4e177e52
Pavel_khlebchick <pavelkhlebchik@gmail.com>
2021-11-22 23:22:54 +0300

    Добавляет новые роуты для приложения

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import {
 
 import HomePage from './routes/HomePage';
 import TasksPage from './routes/TasksPage';
+import NewCardPage from './routes/NewCardPage';
+import CardPage from './routes/CardPage';
 // import NotFoundPage from './routes/NoFoundPage';
 
 const App = () => (
@@ -12,6 +14,8 @@ const App = () => (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/all" element={<TasksPage />} />
+      <Route path="/new" element={<NewCardPage />} />
+      <Route path="/card" element={<CardPage />} />
     </Routes>
   </>
 );
