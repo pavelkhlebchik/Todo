25fc6b35cd84d20a27245e46364b5619d523bede
Pavel_khlebchick <pavelkhlebchik@gmail.com>
2021-11-22 23:22:00 +0300

    добавляет обработчик для добавления новой задачи

--- src/routes/HomePage/index.js
+++ src/routes/HomePage/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useNavigate } from 'react-router';
 
 import Layout from '../../components/Layout';
+import List from '../../components/List';
 
 const HomePage = () => {
   const navigate = useNavigate();
@@ -10,12 +11,18 @@ const HomePage = () => {
     navigate('/all');
   };
 
+  const handleClickAdd = () => {
+    navigate('/new');
+  };
+
   return <>
-        <Layout />
-        <button onClick={handleClickViewAll}>View All</button>
-        <button>Add Todo</button>
-        <span>or</span>
-        <button>Add random Todo :)</button>
+        <Layout>
+            <List />
+            <button onClick={handleClickViewAll}>View All</button>
+            <button onClick={handleClickAdd}>Add Todo</button>
+            <span>or</span>
+            <button>Add random Todo :)</button>
+        </Layout>
     </>;
 };
 
