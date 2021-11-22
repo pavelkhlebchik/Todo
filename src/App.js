d2c8540045e2687fd4fb949c0c518291b5b0af55
Pavel_khlebchick <pavelkhlebchik@gmail.com>
2021-11-22 12:54:02 +0300

    Добавляет роутинг для приложения

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import {
+  Routes, Route,
+} from 'react-router-dom';
+
+import HomePage from './routes/HomePage';
+import TasksPage from './routes/TasksPage';
+// import NotFoundPage from './routes/NoFoundPage';
 
 const App = () => (
-    <div />
+  <>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/all" element={<TasksPage />} />
+    </Routes>
+  </>
 );
 
 export default App;
