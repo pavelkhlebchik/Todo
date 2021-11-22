3bbe3f2a56fd41c8928628cb1207e0ecc538af9a
Pavel_khlebchick <pavelkhlebchik@gmail.com>
2021-11-22 23:20:26 +0300

    Добавляет пропс для рендера html элементов внутри компонента

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import List from '../List';
 
 import s from './layout.module.css';
 
-const Layout = () => (
+// eslint-disable-next-line react/prop-types
+const Layout = ({ children }) => (
     <div className={s.wrapper}>
       <h1 className={s.title}>todo
       <span className={s.title_accent}>list</span>
       </h1>
-      <List />
+      {children}
     </div>
 );
 
