89afd7c0925e0957ed29bf5c25c7dfc7587a6f3d
Pavel_khlebchick <pavelkhlebchik@gmail.com>
2021-11-22 23:13:04 +0300

    Добавляет данные из стора и вешает обработчик на редактирование карточки задачи

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,23 +1,42 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router';
 
-import TASKS from '../../assets/data';
+import { selectTask } from '../../store/selectedTask';
 import Item from '../Item';
 
 import s from './list.module.css';
 
-const List = () => (
-  <div>
+const List = () => {
+  const tasks = useSelector((state) => state.tasks);
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleClick = (task) => {
+    const taskObj = tasks.filter((item) => item.title === task)
+      .map((obj) => obj);
+
+    dispatch(selectTask(taskObj[0]));
+    navigate('/card');
+  };
+
+  return <div>
     <div className={s.flex}>
       {
-        TASKS.map((item) => <Item
+        tasks.map((item) => <Item
           key={item.title}
           status={item.done}
           title={item.title}
           description={item.description}
-          />)
+          value={item.title}
+          handleClickCard={handleClick}
+          listMode={true}
+        />)
       }
     </div>
-  </div>
-);
+  </div>;
+};
 
 export default List;
