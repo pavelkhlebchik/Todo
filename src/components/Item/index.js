ad380678d02a94c07bbc8d2ab7799cc3d10e451e
Pavel_khlebchick <pavelkhlebchik@gmail.com>
2021-11-24 01:27:40 +0300

    Добавляет элемент для отметки о завершении задачи и логику для стилей состояния short/full

--- src/components/Item/index.js
+++ src/components/Item/index.js
@@ -4,7 +4,7 @@ import cn from 'classnames';
 import s from './item.module.css';
 
 const Item = ({
-  handleClickCard, title, description, status, value, listMode, editMode,
+  handleClickCard, title, description, status, value, listMode, viewFullCard,
 }) => {
   const [isDone, setIsDone] = useState(status);
 
@@ -13,12 +13,15 @@ const Item = ({
   const handleClickStatus = () => {
     setIsDone((prevstate) => !prevstate);
   };
-  return <div className={s.task}>
+
+  return <div className={cn(s.task, {
+    [s.task_full_card]: viewFullCard === true,
+  })}>
     <button className={cn(s.task_status, {
       [s.task_status_completed]: isDone === true,
       [s.task_status_not_completed]: isDone === false,
     })}
-      onClick={handleClickStatus}></button>
+      onClick={handleClickStatus}><span className={s.task_checkmark}></span></button>
     <section className={s.task_inner}>
       <h2 className={s.task_title}>{title}</h2>
       <p className={s.task_description}>{description}</p>
@@ -28,20 +31,7 @@ const Item = ({
       && <button
         className={s.task_view_btn}
         onClick={handleClick}
-        value={title}
       >View</button>
-    }
-
-    {
-      editMode
-      && <div>
-        <button>
-          Update
-        </button>
-        <button>
-          Delete
-        </button>
-      </div>
     }
   </div>;
 };
