24c873cee3769b211b1452c36ba9446c322b67f6
Pavel_khlebchick <pavelkhlebchik@gmail.com>
2021-11-24 01:25:38 +0300

    Добавляет логику отрисовки задач в состоянии short/full

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,41 +1,53 @@
-/* eslint-disable no-unused-vars */
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router';
+
+import cn from 'classnames';
 
 import { selectTask } from '../../store/selectedTask';
 import Item from '../Item';
 
 import s from './list.module.css';
 
-const List = () => {
+const List = ({ viewFull = true }) => {
   const tasks = useSelector((state) => state.tasks);
 
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
   const handleClick = (task) => {
-    const taskObj = tasks.filter((item) => item.title === task)
+    const currentTask = tasks.filter((item) => item.title === task)
       .map((obj) => obj);
 
-    dispatch(selectTask(taskObj[0]));
+    dispatch(selectTask(currentTask[0]));
     navigate('/card');
   };
 
-  return <div>
-    <div className={s.flex}>
-      {
-        tasks.map((item) => <Item
-          key={item.title}
-          status={item.done}
+  return <div className={cn(s.wrapper, {
+    [s.full]: viewFull === true,
+  })}>
+    {
+      tasks.map((item) => (viewFull
+        ? <Item
+          key={item.id}
+          status={item.status}
           title={item.title}
           description={item.description}
           value={item.title}
           handleClickCard={handleClick}
           listMode={true}
-        />)
-      }
-    </div>
+          viewFullCard={true}
+        />
+        : <Item
+          key={item.id}
+          status={item.status}
+          title={item.title}
+          description={item.description}
+          value={item.title}
+          handleClickCard={handleClick}
+          listMode={true}
+        />))
+    }
   </div>;
 };
 
